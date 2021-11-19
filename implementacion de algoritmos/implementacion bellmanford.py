031ca8beaab280e6d85817ef64c890b8a80e8061
carlos130702 <72170360+carlos130702@users.noreply.github.com>
2021-11-19 18:36:03 -0500

    close #21

--- implementacion de algoritmos/implementacion bellmanford.py
+++ implementacion de algoritmos/implementacion bellmanford.py
@@ -1,12 +1,9 @@
 def bellmanFord(G, s):
   n = len(G)
-
-  #initialize
   cost = [float('inf')]*n
   cost[s]=0
   path = [-1]*n
 
-  #relax
   for _ in range(n-1):
     for u in range(n):
       for v, w in G[u]:
@@ -14,7 +11,6 @@ def bellmanFord(G, s):
           cost[v] = cost[u]+w
           path[v]=u
   
-  #check negative cycle
   for u in range(n):
     for v, w in G[u]:
       if cost[u]+w<cost[v]:
